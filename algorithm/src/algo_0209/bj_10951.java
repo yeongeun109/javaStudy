4b07fe57a1a0d909043f9e3875b905b9f6fb4951
윤영은 <ounog@naver.com>
2021-02-09 19:32:13 +0900

    bj 10951 A+B -4

---
+++ algorithm/src/algo_0209/bj_10951.java
@@ -0,0 +1,19 @@
+package algo_0209;
+
+import java.util.Scanner;
+
+public class bj_10951 {
+
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int result = a + b;
+			System.out.println(result);
+			
+		}
+	}
+
+}

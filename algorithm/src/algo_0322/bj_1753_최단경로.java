cb89fcc054307329a425402bab1ecb85fe99f03c
윤영은 <ounog@naver.com>
2021-03-24 19:17:37 +0900

    bj 1753 최단경로

---
+++ algorithm/src/algo_0322/bj_1753_최단경로.java
@@ -0,0 +1,66 @@
+package algo_0322;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class bj_1753_최단경로 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int V = Integer.parseInt(in.readLine());
+		
+		
+		List<int[]>[] list = new ArrayList[V + 1];
+		int distance[] = new int[V + 1];
+		
+		for(int i = 1; i <= V; i++) {
+			list[i] = new ArrayList<int[]>();
+			distance[i] = Integer.MAX_VALUE;
+		}
+		
+		StringTokenizer st = null;
+		int M = Integer.parseInt(in.readLine());
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			list[from].add(new int[] {to, cost});
+		}
+
+		st = new StringTokenizer(in.readLine(), " ");
+		int start = Integer.parseInt(st.nextToken()); // 출발점
+		int end = Integer.parseInt(st.nextToken()); // 도착점
+		
+		distance[start] = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[1] - o2[1];
+			}
+		});
+		pq.offer(new int[] {start, 0});
+		
+		while(!pq.isEmpty()) {
+			int[] temp = pq.poll();
+			
+			if(temp[1] > distance[temp[0]])
+				continue;
+			
+			for(int j = 0; j < list[temp[0]].size(); j++) {
+				if(distance[list[temp[0]].get(j)[0]] > temp[1] + list[temp[0]].get(j)[1]) {
+					distance[list[temp[0]].get(j)[0]] = temp[1] + list[temp[0]].get(j)[1];
+					pq.offer(new int[] {list[temp[0]].get(j)[0], distance[list[temp[0]].get(j)[0]]});
+				}
+			}
+		}
+		
+		System.out.println(distance[end]);
+	}
+}

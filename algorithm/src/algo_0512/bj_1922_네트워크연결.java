5706849514b60ccfba3f6da4fdaea30edfe90ec3
윤영은 <ounog@naver.com>
2021-05-14 15:50:12 +0900

    bj 1922 네트워크 연결(크루스칼)

---
+++ algorithm/src/algo_0512/bj_1922_네트워크연결.java
@@ -0,0 +1,84 @@
+package algo_0512;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class bj_1922_네트워크연결 {
+	
+	static int parents[], N, arr[];
+	static ArrayList<Edge> edgeList = new ArrayList<Edge>();
+	
+	static class Edge implements Comparable<Edge>{
+		int x, y;
+		int cost;
+
+		public Edge(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(in.readLine());
+		int M = Integer.parseInt(in.readLine());
+		parents = new int[N + 1];
+		//arr = new int[N + 1];
+		
+		for(int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			edgeList.add(new Edge(from, to, cost));
+		}
+		
+		Collections.sort(edgeList);
+		
+		make();		
+		
+		int result = 0;
+		int count = 0; // 선택한 간선수
+		
+		for(Edge edge : edgeList) {
+			if(union(edge.x, edge.y)) { // 싸이클이 발생하지 않았다면
+				result += edge.cost;
+				if(count++ == N - 1) break;
+			}
+		}
+		System.out.println(result);
+	}
+	
+	static void make() { // 크기가 1인 단위집합 만들기
+		for(int i = 0; i < N; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	static int findSet(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = findSet(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		// 이미 같은 집합인지 조사
+		int aRoot = findSet(a);
+		int bRoot = findSet(b);
+		if(aRoot == bRoot) return false;
+		
+		parents[bRoot] = aRoot;
+		return true;
+	}
+}
